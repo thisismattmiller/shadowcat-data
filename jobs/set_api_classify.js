96e536c8672bae9be4acb36db421cddc54b3f173
Matt Miller <thisismattmiller@gmail.com>
2015-08-31 05:16:33 -0400

    oclc classify

--- jobs/set_api_classify.js
+++ jobs/set_api_classify.js
@@ -29,63 +29,109 @@ db.allBibs(function(bib,cursor,mongoConnection){
 	if (!hasChecked){
 
 
-			//build out the data we would use in classify
-			var data = {
-				_id: bib._id,
-				oclc : [],
-				isbn : [],
-				issn : [],
-				title : false,
-				author : false
-			}
+			// //build out the data we would use in classify
+			// var data = {
+			// 	_id: bib._id,
+			// 	oclc : [],
+			// 	isbn : [],
+			// 	issn : [],
+			// 	title : false,
+			// 	author : false
+			// }
 
-			if (bib['sc:oclc']){
-				for (var x in bib['sc:oclc']){
-					data.oclc.push(bib['sc:oclc'][x])
-				}
-			}
+			// if (bib['sc:oclc']){
+			// 	for (var x in bib['sc:oclc']){
+			// 		data.oclc.push(bib['sc:oclc'][x])
+			// 	}
+			// }
 
-			if (bib['lc:oclc']){
-				for (var x in bib['lc:oclc']){
-					data.oclc.push(bib['lc:oclc'][x])
-				}
-			}
-			if (bib['sc:isbn']){
-				for (var x in bib['sc:isbn']){
-					data.isbn.push(bib['sc:isbn'][x])
-				}
-			}
-			if (bib['sc:issn']){
-				for (var x in bib['sc:issn']){
-					data.issn.push(bib['sc:issn'][x])
-				}
-			}
+			// if (bib['lc:oclc']){
+			// 	for (var x in bib['lc:oclc']){
+			// 		data.oclc.push(bib['lc:oclc'][x])
+			// 	}
+			// }
+			// if (bib['sc:isbn']){
+			// 	for (var x in bib['sc:isbn']){
+			// 		data.isbn.push(bib['sc:isbn'][x])
+			// 	}
+			// }
+			// if (bib['sc:issn']){
+			// 	for (var x in bib['sc:issn']){
+			// 		data.issn.push(bib['sc:issn'][x])
+			// 	}
+			// }
 
-			if (bib['author']){
-				data.author = bib['author']
-			}
-			if (bib['title']){
-				data.title = bib['title']
-			}
+			// if (bib['author']){
+			// 	data.author = bib['author']
+			// }
+			// if (bib['title']){
+			// 	data.title = bib['title']
+			// }
+
+			
+
+			// db.insertAPIClassifyRecord(data,function(err,results){
+
+			// 	totalChanged++
+
+			// 	cursor.resume()
+
+			// 	//console.log(data)
+			// 	//if (results) console.log(results.result)
+
+			// })
+
+			// return true
+
+
 
 			
 
-			db.insertAPIClassifyRecord(data,function(err,results){
+	}else{
 
-				totalChanged++
+		//these are ones that had multi matches from the last run, we found the oclc number but that is it so add it back into be processed
+		if (bib['classify:oclc']){
 
-				cursor.resume()
-
-				//console.log(data)
-				//if (results) console.log(results.result)
-
-			})
-
-			return true
+			if (!bib['classify:creatorLC'] && !bib['classify:fast'] && !bib['classify:lcc']){
 
 
+				//build out the data we would use in classify
+				var data = {
+					_id: bib._id,
+					oclc : bib['classify:oclc'],
+					isbn : [],
+					issn : [],
+					title : false,
+					author : false
+				}
 
-			
+				if (bib['author']){
+					data.author = bib['author']
+				}
+				if (bib['title']){
+					data.title = bib['title']
+				}
+
+				db.insertAPIClassifyRecord(data,function(err,results){
+
+					totalChanged++
+
+					cursor.resume()
+
+					//console.log(data)
+					//if (results) console.log(results.result)
+
+				})
+
+				return true
+
+
+
+			}
+
+
+		}
+
 
 	}
 	
