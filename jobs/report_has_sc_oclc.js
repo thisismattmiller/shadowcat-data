407327f1a693875fae3936ac3b502b1e5126ab43
Matt Miller <thisismattmiller@gmail.com>
2015-09-08 16:56:31 -0400

    sc oclc

---
+++ jobs/report_has_sc_oclc.js
@@ -0,0 +1,33 @@
+#!/usr/local/bin/node
+
+var db = require("../lib/db.js")
+var util = require("../lib/util.js")
+
+var counter = 0
+var hasOclc = 0
+
+db.allBibs(function(bib,cursor,mongoConnection){
+
+	process.stdout.clearLine()
+	process.stdout.cursorTo(0)
+	process.stdout.write( counter + " | " +  hasOclc )
+
+	counter++
+
+	if (!bib) console.log("DONE!", counter, hasOclc)
+
+	var oclcFlag = false
+
+
+	if (bib['sc:oclc']){
+		if (bib['sc:oclc'].length > 0) oclcFlag = true
+	}
+
+	if (oclcFlag) hasOclc++
+		
+	cursor.resume()
+
+
+})
+
+
