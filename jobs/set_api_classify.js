829bb9c686f26e97d443a108c7eb95a4a9905b6a
Matt Miller <thisismattmiller@gmail.com>
2015-09-08 10:52:46 -0400

    lcc coarse

--- jobs/set_api_classify.js
+++ jobs/set_api_classify.js
@@ -29,59 +29,59 @@ db.allBibs(function(bib,cursor,mongoConnection){
 	if (!hasChecked){
 
 
-			// //build out the data we would use in classify
-			// var data = {
-			// 	_id: bib._id,
-			// 	oclc : [],
-			// 	isbn : [],
-			// 	issn : [],
-			// 	title : false,
-			// 	author : false
-			// }
+			//build out the data we would use in classify
+			var data = {
+				_id: bib._id,
+				oclc : [],
+				isbn : [],
+				issn : [],
+				title : false,
+				author : false
+			}
 
-			// if (bib['sc:oclc']){
-			// 	for (var x in bib['sc:oclc']){
-			// 		data.oclc.push(bib['sc:oclc'][x])
-			// 	}
-			// }
+			if (bib['sc:oclc']){
+				for (var x in bib['sc:oclc']){
+					data.oclc.push(bib['sc:oclc'][x])
+				}
+			}
 
-			// if (bib['lc:oclc']){
-			// 	for (var x in bib['lc:oclc']){
-			// 		data.oclc.push(bib['lc:oclc'][x])
-			// 	}
-			// }
-			// if (bib['sc:isbn']){
-			// 	for (var x in bib['sc:isbn']){
-			// 		data.isbn.push(bib['sc:isbn'][x])
-			// 	}
-			// }
-			// if (bib['sc:issn']){
-			// 	for (var x in bib['sc:issn']){
-			// 		data.issn.push(bib['sc:issn'][x])
-			// 	}
-			// }
+			if (bib['lc:oclc']){
+				for (var x in bib['lc:oclc']){
+					data.oclc.push(bib['lc:oclc'][x])
+				}
+			}
+			if (bib['sc:isbn']){
+				for (var x in bib['sc:isbn']){
+					data.isbn.push(bib['sc:isbn'][x])
+				}
+			}
+			if (bib['sc:issn']){
+				for (var x in bib['sc:issn']){
+					data.issn.push(bib['sc:issn'][x])
+				}
+			}
 
-			// if (bib['author']){
-			// 	data.author = bib['author']
-			// }
-			// if (bib['title']){
-			// 	data.title = bib['title']
-			// }
+			if (bib['author']){
+				data.author = bib['author']
+			}
+			if (bib['title']){
+				data.title = bib['title']
+			}
 
 			
 
-			// db.insertAPIClassifyRecord(data,function(err,results){
+			db.insertAPIClassifyRecord(data,function(err,results){
 
-			// 	totalChanged++
+				totalChanged++
 
-			// 	cursor.resume()
+				cursor.resume()
 
-			// 	//console.log(data)
-			// 	//if (results) console.log(results.result)
+				//console.log(data)
+				//if (results) console.log(results.result)
 
-			// })
+			})
 
-			// return true
+			return true
 
 
 
@@ -118,8 +118,6 @@ db.allBibs(function(bib,cursor,mongoConnection){
 
 					cursor.resume()
 
-					//console.log(data)
-					//if (results) console.log(results.result)
 
 				})
 
--- jobs/set_coarse_lcc.js
+++ jobs/set_coarse_lcc.js
@@ -4,45 +4,130 @@ var db = require("../lib/db.js")
 var util = require("../lib/util.js")
 
 var counter = 0
+var totalChanged = 0
 
 var log = require('simple-node-logger').createSimpleLogger(__dirname + '/../log/coarse_lcc_error.log');
 
-
-util.parseLCC(function(lcc){
-
+util.parseLCC(function(allLccs){
 
 	db.allBibs(function(bib,cursor,mongoConnection){
 
 		process.stdout.clearLine()
 		process.stdout.cursorTo(0)
-		process.stdout.write( counter + "" )
+		process.stdout.write( counter + " | " + totalChanged + " | " )
 
 		counter++
 
+		if (!bib['classify:lcc'] || bib['sc:lccCoarse'] === false){
 
-		if (bib['classify:lcc']){
+
+			//see if we have the lcc in the 050		
+			var lcc = false, lcc908 = false, possibleLcc = []
 
 
-			var updateRecord  = {
-				id : bib.id,
-				"sc:lccCoarse" : util.coarseLCC(bib['classify:lcc'],lcc)
+			for (var field in bib.varFields){
+				if (bib.varFields[field].marcTag == '050') lcc = bib.varFields[field]
+				if (bib.varFields[field].marcTag == '908') lcc908 = bib.varFields[field]
+			}
+
+			//if (!lcc && lccn) console.log("|",bib.id, lcc,lccn )
+
+			if (lcc){
+
+				var subfieldA = false
+				var allfields = false
+
+				for (var x in lcc.subfields){
+					if (lcc.subfields[x].tag==='a'){
+						 if (lcc.subfields[x]['content']) {
+						 	subfieldA = lcc.subfields[x]['content']
+						 	allfields = lcc
+						 }
+					}
+				}
+
+				if (subfieldA) possibleLcc.push(subfieldA)
+
+			}
+			if (lcc908){
+				var subfieldA = false
+				var allfields = false
+				for (var x in lcc908.subfields){
+					if (lcc908.subfields[x].tag==='a'){
+						 if (lcc908.subfields[x]['content']) {
+						 	subfieldA = lcc908.subfields[x]['content']
+						 	allfields = lcc908
+						 }
+					}
+				}
+				if (subfieldA) possibleLcc.push(subfieldA)
 			}
 
 
+			//see if we have it from the LCCN api
+
+			if (bib['lc:lcc']){
+				possibleLcc.push(bib['lc:lcc'])
+			}
+
+
+
+			for (var x in possibleLcc){
+
+
+			
+				var coarse = util.coarseLCC(possibleLcc[x],allLccs)
+
+				if (coarse){
+
+					var updateRecord  = {
+						id : bib.id,
+						"sc:lccCoarse" : coarse
+					}
+
+					db.updateBibRecord(updateRecord,function(err,r){
+						if (err) console.log("ERRROR:",err)
+						totalChanged++
+						cursor.resume()
+						return
+					}, mongoConnection)
+
+
+
+				}else{
+					log.info(possibleLcc[x])
+				}
+
+
+			}
+
+			cursor.resume()
+
+
+
+
+
+			
+
+		}else{
+
+
+
+			//if it has the classify LCC use that instead
+			var updateRecord  = {
+				id : bib.id,
+				"sc:lccCoarse" : util.coarseLCC(bib['classify:lcc'],allLccs)
+			}
+
+
+			if (!updateRecord["sc:lccCoarse"]) log.info(bib['classify:lcc'])
 			db.updateBibRecord(updateRecord,function(err,r){
 
-				if (err) console.log("ERRROR:",err)
-
-				console.log(updateRecord)
-				console.log(r.result)
+			 	if (err) console.log("ERRROR:",err)
 				cursor.resume()
 
 			}, mongoConnection)
 
-
-		}else{
-
-			cursor.resume()
 			
 		}
 
@@ -51,7 +136,5 @@ util.parseLCC(function(lcc){
 
 	})
 
-
-
-
 })
+
